b6744debfd6a230e78e2dc14d694d378e3b5b327
Geovanny Olimpio <geovannyolimpio@gmail.com>
2016-07-26 17:06:52 -0300

    autenticação do Usuário

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  
   resources :users
   root 'static_page#index'
 
   get 'sobre', to: 'static_page#sobre'
   get 'contato', to: 'static_page#contato'
+
+  get 'entrar', to: 'sessions#new'
+  post 'entrar', to: 'sessions#create'
+  delete 'sair', to: 'sessions#destroy'
 
 
 end
